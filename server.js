dd3a352cd70c6db17b111316665c3e1f84b808fa
Zack <zinouye@gmail.com>
2023-08-13 22:27:11 -0700

    added a route

--- server.js
+++ server.js
@@ -6,9 +6,18 @@ const cors = require('cors')
 
 const app = express()
 
-const PORT = process.env.PORT || 3001
+// Middlewares
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+// Routes
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 
 // Connect to MongoDB and start server
+const PORT = process.env.PORT || 3001
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))

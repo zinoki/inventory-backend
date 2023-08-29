9228ff9d6e03c7ca842218ba02181c753b569086
Zack <zinouye@gmail.com>
2023-08-29 11:43:39 +0700

    generating token with jsonwebtoken

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,10 @@
 const asyncHandler = require('express-async-handler')
 const User = require('../models/userModel')
+const jwt = require('jsonwebtoken')
+
+const generateToken = (id) => {
+  return jwt.sign({id}, process.env.JWT_SECRET, {expiresIn: "1d"})
+}
 
 const registerUser = asyncHandler(async (req,res) => {
   const {name, email, password} = req.body
@@ -22,13 +27,18 @@ const registerUser = asyncHandler(async (req,res) => {
     throw new Error("Email has already been registered")
   }  
 
-
+  
+  
+  
   // Create new user
   const user = await User.create({
     name,
     email,
     password
   })
+
+  // Generate Token
+  const token = generateToken(user._id)
 
   if (user) {
     const { _id, name, email, photo, phone, bio } = user
@@ -38,7 +48,8 @@ const registerUser = asyncHandler(async (req,res) => {
       email,
       photo,
       phone,
-      bio
+      bio,
+      token
     })
   } else {
     res.status(400)

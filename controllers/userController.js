73aabeef1ca009b60594fea94bf2716d4e21d33d
Zack <zinouye@gmail.com>
2023-08-29 12:33:13 +0700

    sending token with  http only cookie

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,7 @@
 const asyncHandler = require('express-async-handler')
 const User = require('../models/userModel')
 const jwt = require('jsonwebtoken')
+const cookieParser = require('cookie-parser')
 
 const generateToken = (id) => {
   return jwt.sign({id}, process.env.JWT_SECRET, {expiresIn: "1d"})
@@ -26,9 +27,6 @@ const registerUser = asyncHandler(async (req,res) => {
     res.status(400)
     throw new Error("Email has already been registered")
   }  
-
-  
-  
   
   // Create new user
   const user = await User.create({
@@ -39,6 +37,15 @@ const registerUser = asyncHandler(async (req,res) => {
 
   // Generate Token
   const token = generateToken(user._id)
+
+  // Send HTTP-only cookie
+  res.cookie("token", token, {
+    path: "/",
+    httpOnly: true,
+    expires: new Date(Date.now()+1000*86400), // 1 day
+    sameSite: "none",
+    secure: true
+  })
 
   if (user) {
     const { _id, name, email, photo, phone, bio } = user
--- server.js
+++ server.js
@@ -3,14 +3,16 @@ const express = require('express')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 const cors = require('cors')
-
 const userRoute = require('./routes/userRoute')
 const errorHandler = require('./middleware/errorMiddleware')
+const cookieParser = require('cookie-parser')
+
 // initialize express
 const app = express()
 
 // Middlewares
 app.use(express.json())
+app.use(cookieParser())
 app.use(express.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 

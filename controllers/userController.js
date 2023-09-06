33f93d90e8bd51b36522c95f27c43b98c5b4181c
Zack <zinouye@gmail.com>
2023-09-06 14:39:05 +0700

    create update user controller function

--- controllers/userController.js
+++ controllers/userController.js
@@ -175,7 +175,30 @@ const loginStatus = asyncHandler(
 // Update User
 const updateUser = asyncHandler(
   async (req, res) => {
-    res.send("user updated")
+    const user = await User.findById(req.user._id)
+    if (user) {
+      const { name, email, photo, phone, bio } = user
+      user.email = email
+      user.name = req.body.name || name
+      user.email = req.body.email || email
+      user.photo = req.body.photo || photo
+      user.phone = req.body.phone || phone
+      user.bio = req.body.bio || bio
+      const updatedUser = await user.save()
+     
+      res.status(200).json({
+        _id: updatedUser._id,
+        name: updatedUser.name,
+        email: updatedUser.email,
+        photo: updatedUser.photo,
+        phone: updatedUser.phone,
+        bio: updatedUser.bio
+      })
+    } else {
+      res.status(404)
+      throw new Error("User not found")
+    }
+
   }
 )
 module.exports = {

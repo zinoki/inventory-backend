28d91c3395be527dd78f3f24923549b19b98459e
Zack <zinouye@gmail.com>
2023-09-10 01:34:28 +0700

    set up multer file upload

---
+++ utils/uploadFile.js
@@ -0,0 +1,29 @@
+const multer = require("multer")
+
+// Define file storage
+const storage = multer.diskStorage({
+  destination: function(req, file, cb) {
+    cb(null, 'uploads')
+  },
+  filename: function(req, file, cb) {
+    cb(null, Date().toISOString().replace(/:/g, "-") + file.originalname)
+  }
+})
+
+// Specify file format that can be saved
+function fileFilter (req, file, cb) {
+  if(
+    file.mimetype === "image/png" ||
+    file.mimetype === "image/jpg" ||
+    file.mimetype === "image/jpeg"
+  ) {
+    cb(null, true)
+  } else {
+    cb(null, false)
+  }
+}
+
+
+const upload = multer({ storage, fileFilter })
+
+module.exports = upload

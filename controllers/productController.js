5579fb16efe8c8356f42a57decfb85c51a48f0d7
Zack <zinouye@gmail.com>
2023-09-10 01:08:01 +0700

    created product controller

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,9 +1,31 @@
 const asyncHandler = require("express-async-handler");
+const Product = require("../models/productModel");
 
 const createProduct = asyncHandler(
   async(req, res) => {
-    res.send("Product created")
+    const { name, sku, category, quantity, price, description } = req.body
+    // Validation 
+    if (!name ||!category || !quantity || !price ||!description) {
+      res.status(400)
+      throw new Error("Please fill in all fields")
+    }
+
+    // Manage image upload
+
+    // Create product
+    const product = await Product.create({
+      user: req.user.id,
+      name,
+      sku,
+      category,
+      quantity,
+      price,
+      description
+    })
+    res.status(201).json(product)
+
   }
+
 )
 
 module.exports = {

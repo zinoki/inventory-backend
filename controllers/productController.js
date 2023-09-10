dfc91edb511e89087d90018c27d9650ae80a495b
Zack <zinouye@gmail.com>
2023-09-10 13:04:44 +0700

    get all products route and controller

--- controllers/productController.js
+++ controllers/productController.js
@@ -50,6 +50,16 @@ const createProduct = asyncHandler(
 
 )
 
+// Get all products
+
+const getProducts = asyncHandler(
+  async (req, res) => {
+    const products = await Product.find({user: req.user.id}).sort("-createdAt")
+    res.status(200).json(products)
+  }
+)
+
 module.exports = {
-  createProduct
+  createProduct,
+  getProducts
 }
--- routes/productRoute.js
+++ routes/productRoute.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const protect = require('../middleware/authMiddleware')
-const { createProduct } = require('../controllers/productController')
+const { createProduct, getProducts } = require('../controllers/productController')
 const router = express.Router()
 const { upload } = require('../utils/uploadFile')
 
 router.post("/", protect, upload.single("image"), createProduct)
+router.get("/", protect, getProducts)
 
 module.exports = router

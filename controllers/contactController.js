698b5456ca4828de77c00bba307330222957a061
Zack <zinouye@gmail.com>
2023-09-10 15:36:06 +0700

    created contact controller

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -1,8 +1,36 @@
 const asyncHandler = require("express-async-handler");
+const User = require("../models/userModel");
+const sendEmail = require("../utils/sendEmail");
+
 
 const contactUs = asyncHandler(
   async (req, res) => {
-    res.send("Contact Us")
+    const { subject, message } = req.body
+    const user = await User.findById(req.user._id)
+
+    if (!user) {
+      res.status(400)
+      throw new Error("User not found. Please sign up")
+    }
+
+    // Validation
+    if (!subject || !message) {
+      res.status(400)
+      throw new Error("Please add subject and message")
+    }
+
+    const send_to = process.env.EMAIL_USER
+    const sent_from = process.env.Email_USER
+    const reply_to = user.email
+
+
+    try {
+      await sendEmail(subject, message, send_to, sent_from, reply_to)
+      res.status(200).json({ success: true, message: "Email sent."})
+    } catch(err) {
+      res.status(500)
+      throw new Error("Email not sent. Please try again")
+    }
   }
 )
 

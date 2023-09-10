078d38d1a24442df5c4437acdfb8918a3df82814
Zack <zinouye@gmail.com>
2023-09-10 13:11:32 +0700

    get a single product route and controller

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const protect = require('../middleware/authMiddleware')
-const { createProduct, getProducts } = require('../controllers/productController')
+const { createProduct, getProducts, getProduct } = require('../controllers/productController')
 const router = express.Router()
 const { upload } = require('../utils/uploadFile')
 
 router.post("/", protect, upload.single("image"), createProduct)
 router.get("/", protect, getProducts)
+router.get("/:id", protect, getProduct)
 
 module.exports = router

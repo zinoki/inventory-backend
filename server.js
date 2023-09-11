d0b8d10950a3addbf85241dc3113bd32450621cf
Zack <zinouye@gmail.com>
2023-09-11 14:25:31 +0700

    updated cors

--- server.js
+++ server.js
@@ -19,7 +19,10 @@ app.use(express.json())
 app.use(cookieParser())
 app.use(express.urlencoded({ extended: false }))
 app.use(bodyParser.json())
-app.use(cors())
+app.use(cors({
+  origin: ["http://localhost:3000", "https://inventoryapex.com"], //fix later
+  credentials: true
+}))
 
 app.use("/uploads", express.static(path.join(__dirname, "uploads")))
 

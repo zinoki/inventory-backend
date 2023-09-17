af5a3704e0dc8b998fe290f8e20cf97247d3d1e9
Zack <zinouye@gmail.com>
2023-09-17 11:04:33 +0700

    modified errorHandler again

--- middleware/errorMiddleware.js
+++ middleware/errorMiddleware.js
@@ -1,26 +1,26 @@
-// const errorHandler = (err, req, res, next) => {
-//   if (res.headersSent) {
-//     return next(err);
-//   }
-  
-//   const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
-//   res.status(statusCode);
-
-//   res.json({
-//     message: err.message,
-//     stack: process.env.NODE_ENV === 'development' ? err.stack : null
-//   });
-// }
-
 const errorHandler = (err, req, res, next) => {
-  const statusCode = res.statusCode ? res.statusCode : 500
-  res.status(statusCode)
+  if (res.headersSent) {
+    return next(err);
+  }
+  
+  const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+  res.status(statusCode);
 
   res.json({
     message: err.message,
     stack: process.env.NODE_ENV === 'development' ? err.stack : null
-  })
+  });
 }
+
+// const errorHandler = (err, req, res, next) => {
+//   const statusCode = res.statusCode ? res.statusCode : 500
+//   res.status(statusCode)
+
+//   res.json({
+//     message: err.message,
+//     stack: process.env.NODE_ENV === 'development' ? err.stack : null
+//   })
+// }
 
 
 
